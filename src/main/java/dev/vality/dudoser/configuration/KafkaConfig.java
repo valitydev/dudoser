44a18692886846c373647dd1af877873145c9326
Egor Cherniak <cherniak3@yandex.ru>
2022-09-07 18:26:26 +0300

    Add new kafka config + iam support (#15)
    * Add new kafka config + iam support
    
    * Restore consumerFactory

--- src/main/java/dev/vality/dudoser/configuration/KafkaConfig.java
+++ src/main/java/dev/vality/dudoser/configuration/KafkaConfig.java
@@ -1,20 +1,18 @@
 package dev.vality.dudoser.configuration;
 
 import dev.vality.damsel.payment_processing.EventPayload;
-import dev.vality.dudoser.configuration.properties.KafkaSslProperties;
 import dev.vality.dudoser.serde.SinkEventDeserializer;
+import dev.vality.kafka.common.util.ExponentialBackOffDefaultErrorHandlerFactory;
 import dev.vality.machinegun.eventsink.MachineEvent;
 import dev.vality.sink.common.parser.impl.MachineEventParser;
 import dev.vality.sink.common.parser.impl.PaymentEventPayloadMachineEventParser;
 import dev.vality.sink.common.serialization.BinaryDeserializer;
 import dev.vality.sink.common.serialization.impl.PaymentEventPayloadDeserializer;
-import org.apache.kafka.clients.CommonClientConfigs;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.config.SslConfigs;
-import org.apache.kafka.common.security.auth.SecurityProtocol;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -23,73 +21,30 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.kafka.listener.ErrorHandler;
-import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
-import org.springframework.util.backoff.FixedBackOff;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 
-import java.io.File;
-import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@EnableConfigurationProperties(KafkaSslProperties.class)
+@RequiredArgsConstructor
 public class KafkaConfig {
 
-    @Value("${kafka.consumer.auto-offset-reset}")
-    private String autoOffsetReset;
-    @Value("${kafka.consumer.enable-auto-commit}")
-    private boolean enableAutoCommit;
-    @Value("${kafka.consumer.group-id}")
-    private String groupId;
-    @Value("${kafka.client-id}")
-    private String clientId;
-    @Value("${kafka.consumer.max-poll-records}")
-    private int maxPollRecords;
-
-    @Value("${kafka.bootstrap-servers}")
-    private String bootstrapServers;
     @Value("${kafka.consumer.concurrency}")
     private int concurrency;
 
-
-    @Value("${retry-policy.maxAttempts}")
-    int maxAttempts;
+    private final KafkaProperties kafkaProperties;
 
     @Bean
-    public Map<String, Object> consumerConfigs(KafkaSslProperties kafkaSslProperties) {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = kafkaProperties.buildConsumerProperties();
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SinkEventDeserializer.class);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.CLIENT_ID_CONFIG, clientId);
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, enableAutoCommit);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
-        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, maxPollRecords);
-
-        configureSsl(props, kafkaSslProperties);
-
         return props;
     }
 
-    private void configureSsl(Map<String, Object> props, KafkaSslProperties kafkaSslProperties) {
-        if (kafkaSslProperties.isEnabled()) {
-            props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name());
-            props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,
-                    new File(kafkaSslProperties.getTrustStoreLocation()).getAbsolutePath());
-            props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, kafkaSslProperties.getTrustStorePassword());
-            props.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, kafkaSslProperties.getKeyStoreType());
-            props.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, kafkaSslProperties.getTrustStoreType());
-            props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG,
-                    new File(kafkaSslProperties.getKeyStoreLocation()).getAbsolutePath());
-            props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, kafkaSslProperties.getKeyStorePassword());
-            props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, kafkaSslProperties.getKeyPassword());
-        }
-    }
-
     @Bean
-    public ConsumerFactory<String, MachineEvent> consumerFactory(KafkaSslProperties kafkaSslProperties) {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs(kafkaSslProperties));
+    public ConsumerFactory<String, MachineEvent> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
     }
 
     @Bean
@@ -100,13 +55,13 @@ public class KafkaConfig {
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
-        factory.setErrorHandler(kafkaErrorHandler());
+        factory.setCommonErrorHandler(kafkaErrorHandler());
         factory.setConcurrency(concurrency);
         return factory;
     }
 
-    private ErrorHandler kafkaErrorHandler() {
-        return new SeekToCurrentErrorHandler(new FixedBackOff(0, maxAttempts));
+    private DefaultErrorHandler kafkaErrorHandler() {
+        return ExponentialBackOffDefaultErrorHandlerFactory.create();
     }
 
     @Bean
